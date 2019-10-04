6776167c3472baf11e7fc002f3d64be9502ee379
Algorithm <49584601+algorithmsevolve@users.noreply.github.com>
2019-10-04 09:34:21 +0700

    Update #8.1
    Added newest database

--- database/rsjsh-db.sql
+++ database/rsjsh-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Sep 2019 pada 13.59
+-- Waktu pembuatan: 04 Okt 2019 pada 04.32
 -- Versi server: 10.1.32-MariaDB
 -- Versi PHP: 5.6.36
 
@@ -46,7 +46,13 @@ CREATE TABLE `komentar` (
 --
 
 INSERT INTO `komentar` (`id_komentar`, `id_user`, `nama_user`, `divisi_user`, `tanggal`, `komentar`, `nomor_laporan`, `status_komentar`) VALUES
-('K-L-b/Kenangan/UGD/28092019/001-001', '696969', 'Akbar Riski', 'Pegawai', '2019-09-28', 'haha', 'L-b/Kenangan/UGD/28092019/001', '0');
+('K-L-a/dsgag/sdagasdg/02102019/001-001', '007', 'Alma Adela', 'Teknisi', '2019-10-02', 'test', 'L-a/dsgag/sdagasdg/02102019/001', '1'),
+('K-L-a/dsgag/sdagasdg/02102019/001-002', '007', 'Alma Adela', 'Teknisi', '2019-10-02', 'sdgsdga', 'L-a/dsgag/sdagasdg/02102019/001', '1'),
+('K-L-a/dsgag/sdagasdg/02102019/001-003', '12346', 'Angga Nugraha', 'Teknisi', '2019-10-02', 'aku kangen kamu adella', 'L-a/dsgag/sdagasdg/02102019/001', '0'),
+('K-L-b/Kenangan/UGD/28092019/001-001', '696969', 'Akbar Riski', 'Pegawai', '2019-09-28', 'haha', 'L-b/Kenangan/UGD/28092019/001', '1'),
+('K-L-b/Kenangan/UGD/28092019/001-002', '12345', 'Viki Hardiyanto', 'Pegawai', '2019-10-02', 'test', 'L-b/Kenangan/UGD/28092019/001', '0'),
+('K-L-b/Kenangan/UGD/28092019/001-003', '12345', 'Viki Hardiyanto', 'Pegawai', '2019-10-02', 'bacot', 'L-b/Kenangan/UGD/28092019/001', '0'),
+('K-L-b/Kenangan/UGD/28092019/001-004', '696969', 'Akbar Riski', 'Pegawai', '2019-10-02', 'yee otong', 'L-b/Kenangan/UGD/28092019/001', '0');
 
 -- --------------------------------------------------------
 
@@ -64,15 +70,18 @@ CREATE TABLE `laporan_air` (
   `nip_teknisi` varchar(255) NOT NULL,
   `keluhan` varchar(256) NOT NULL,
   `tambahan` text NOT NULL,
-  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL
+  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL,
+  `barang_yang_diganti` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `laporan_air`
 --
 
-INSERT INTO `laporan_air` (`nomor_laporan`, `tanggal`, `ruangan`, `bagian`, `id_pelapor`, `pelapor`, `nip_teknisi`, `keluhan`, `tambahan`, `status`) VALUES
-('L-a/Kenangan/UGD/28092019/001', '2019-09-28', 'Kenangan', 'UGD', '696969', 'Akbar Riski', '12346', 'Air Tidak Mengalir   ', 'haha', 'Selesai');
+INSERT INTO `laporan_air` (`nomor_laporan`, `tanggal`, `ruangan`, `bagian`, `id_pelapor`, `pelapor`, `nip_teknisi`, `keluhan`, `tambahan`, `status`, `barang_yang_diganti`) VALUES
+('L-a/asdgs/gasdag/02102019/001', '2019-10-02', 'asdgs', 'gasdag', '696969', 'Akbar Riski', '', ' Air Keruh/Berwarna  ', 'asdgag', 'Menunggu Konfirmasi', ''),
+('L-a/dsgag/sdagasdg/02102019/001', '2019-10-02', 'dsgag', 'sdagasdg', '696969', 'Akbar Riski', '007', 'Air Tidak Mengalir   ', 'gasdgas', 'Selesai', 'aelah bacot'),
+('L-a/Kenangan/UGD/28092019/001', '2019-09-28', 'Kenangan', 'UGD', '696969', 'Akbar Riski', '12346', 'Air Tidak Mengalir   ', 'haha', 'Selesai', '');
 
 -- --------------------------------------------------------
 
@@ -90,15 +99,18 @@ CREATE TABLE `laporan_bangunan` (
   `nip_teknisi` varchar(255) NOT NULL,
   `keluhan` varchar(256) NOT NULL,
   `tambahan` text NOT NULL,
-  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL
+  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL,
+  `barang_yang_diganti` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `laporan_bangunan`
 --
 
-INSERT INTO `laporan_bangunan` (`nomor_laporan`, `tanggal`, `ruangan`, `bagian`, `id_pelapor`, `pelapor`, `nip_teknisi`, `keluhan`, `tambahan`, `status`) VALUES
-('L-b/Kenangan/UGD/28092019/001', '2019-09-28', 'Kenangan', 'UGD', '696969', 'Akbar Riski', '12346', 'Atap Bocor  ', 'gagsa', 'Sedang Dikerjakan');
+INSERT INTO `laporan_bangunan` (`nomor_laporan`, `tanggal`, `ruangan`, `bagian`, `id_pelapor`, `pelapor`, `nip_teknisi`, `keluhan`, `tambahan`, `status`, `barang_yang_diganti`) VALUES
+('L-b/dgas/gdsaga/02102019/001', '2019-10-02', 'dgas', 'gdsaga', '696969', 'Akbar Riski', '', 'Atap Bocor  ', 'dgasd', 'Menunggu Konfirmasi', ''),
+('L-b/gdg/gadg/02102019/001', '2019-10-02', 'gdg', 'gadg', '696969', 'Akbar Riski', '', 'Atap Bocor  ', 'gdasg', 'Menunggu Konfirmasi', ''),
+('L-b/Kenangan/UGD/28092019/001', '2019-09-28', 'Kenangan', 'UGD', '696969', 'Akbar Riski', '7', 'Atap Bocor  ', 'gagsa', 'Selesai', 'ganti pacar');
 
 -- --------------------------------------------------------
 
@@ -117,7 +129,8 @@ CREATE TABLE `laporan_furnitur` (
   `keluhan` varchar(256) NOT NULL,
   `jenis_keluhan` varchar(256) NOT NULL,
   `tambahan` varchar(256) NOT NULL,
-  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL
+  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL,
+  `barang_yang_diganti` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -136,8 +149,17 @@ CREATE TABLE `laporan_listrik` (
   `nip_teknisi` varchar(255) NOT NULL,
   `keluhan` varchar(256) NOT NULL,
   `tambahan` text NOT NULL,
-  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL
+  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL,
+  `barang_yang_diganti` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `laporan_listrik`
+--
+
+INSERT INTO `laporan_listrik` (`nomor_laporan`, `tanggal`, `ruangan`, `bagian`, `id_pelapor`, `pelapor`, `nip_teknisi`, `keluhan`, `tambahan`, `status`, `barang_yang_diganti`) VALUES
+('L-l/asdgasd/gdsgasd/02102019/001', '2019-10-02', 'asdgasd', 'gdsgasd', 696969, 'Akbar Riski', '', '   ', 'agdasg', 'Menunggu Konfirmasi', ''),
+('L-l/asdsa/gsdaa/02102019/001', '2019-10-02', 'asdsa', 'gsdaa', 696969, 'Akbar Riski', '', ' Kabel Rusak  ', 'gsdag', 'Menunggu Konfirmasi', '');
 
 -- --------------------------------------------------------
 
@@ -156,7 +178,8 @@ CREATE TABLE `laporan_peralatan` (
   `nama_peralatan` varchar(256) NOT NULL,
   `jenis_keluhan` varchar(256) NOT NULL,
   `tambahan` varchar(256) NOT NULL,
-  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL
+  `status` enum('Menunggu Konfirmasi','Sedang Dikerjakan','Selesai') NOT NULL,
+  `barang_yang_diganti` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -166,7 +189,7 @@ CREATE TABLE `laporan_peralatan` (
 --
 
 CREATE TABLE `user` (
-  `NIP` int(255) NOT NULL,
+  `NIP` varchar(255) NOT NULL,
   `nama_user` varchar(256) NOT NULL,
   `divisi` varchar(256) NOT NULL,
   `ruangan` varchar(256) NOT NULL,
@@ -179,9 +202,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`NIP`, `nama_user`, `divisi`, `ruangan`, `instalasi`, `password`) VALUES
-(12345, 'Viki Hardiyanto', 'pegawai', 'Merak', 'Unit Gawat Darurat', 'ojan21'),
-(12346, 'Angga Nugraha', 'teknisi', '', '', 'anggabokep'),
-(696969, 'Akbar Riski', 'pegawai', 'Kenangan', 'Install Game', 'anggabokep');
+('007', 'Alma Adela', 'teknisi', '', '', 'kangenangga'),
+('12345', 'Viki Hardiyanto', 'pegawai', 'Merak', 'Unit Gawat Darurat', 'ojan21'),
+('12346', 'Angga Nugraha', 'teknisi', '', '', 'anggabokep'),
+('696969', 'Akbar Riski', 'pegawai', 'Kenangan', 'Install Game', 'anggabokep');
 
 --
 -- Indexes for dumped tables
